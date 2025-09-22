16f6a315e571704d7fa3e8610b23010127c97893
DereIBims <philip.kunes@me.com>
2025-09-22 22:55:06 +0200

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,13 @@
 from sqlmodel import SQLModel, create_engine, Session
+from pathlib import Path
 
-DATABASE_URL = "sqlite:///./data/filament.db"
+DB_DIR  = Path("./data")
+DB_FILE = DB_DIR / "filament.db"
+
+DB_DIR.mkdir(parents=True, exist_ok=True)
+
+DATABASE_URL = f"sqlite:///{DB_FILE}"
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 def init_db():
